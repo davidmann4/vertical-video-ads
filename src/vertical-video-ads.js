d6b9401d9c576e9296a5a2a9d287428f67a87ad0
David Mann <daaavid.mann@gmail.com>
2016-06-19 08:55:02 -0700

    basic video player

--- src/vertical-video-ads.js
+++ src/vertical-video-ads.js
@@ -1,2 +1,13 @@
 /*! vertical-video-ads | (c) David Mann | github.com/davidmann4/vertical-video-ads */
 
+//usage: <script src="vertical-video-ads.js"/><div class="vertical-video-ad" data-ad-src="PATH-TO-MP4"/>
+
+
+(function($){
+  $(".vertical-video-ad").each(function(){
+    video = $("<video>")
+    video.append($("<source>").attr("type", "video/mp4").attr("src", $(this).attr("data-ad-src")))
+    $(this).append(video);    
+  });
+})(jQuery);
+
