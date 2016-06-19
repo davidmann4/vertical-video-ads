0c63039a1133e2bffee6b24d38fccab4c08ff546
David Mann <daaavid.mann@gmail.com>
2016-06-19 08:39:24 -0700

    gruntfile for concat / uglify

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['src/jquery-3.0.0.min.js', 'src/vertical-video-ads.js'],
+        // the location of the resulting JS file
+        dest: 'lib/<%= pkg.name %>.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
