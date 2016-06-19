1244d46bb7c2c7200cd975d511c363537ada0c99
David Mann <daaavid.mann@gmail.com>
2016-06-19 10:41:10 -0700

    basic prototype

--- src/vertical-video-ads.js
+++ src/vertical-video-ads.js
@@ -4,10 +4,23 @@
 
 
 (function($){
-  $(".vertical-video-ad").each(function(){
-    video = $("<video>")
-    video.append($("<source>").attr("type", "video/mp4").attr("src", $(this).attr("data-ad-src")))
-    $(this).append(video);    
+  $( document ).ready(function() {
+    $(".vertical-video-ad").each(function(){
+      wrapper = $("<div>").addClass("vertical-video-ad-wrapper");
+      video = $("<video>").attr("id", "vertical-video-ad000");
+      video.append($("<source>").attr("type", "video/mp4").attr("src", $(this).attr("data-ad-src")))
+      $(this).append($(wrapper).append(video));  
+      video.get(0).play();  
+
+      //.vertical-video-ad video
+
+      $(window).scroll(function() {
+      	calculatedOffset = $(window).scrollTop() - $('#vertical-video-ad000').parent().offset().top;
+      	if (calculatedOffset < 0) {
+      		$("#vertical-video-ad000").css("top",calculatedOffset);
+      	}      	
+      });
+    });
   });
 })(jQuery);
 
